0ea506922cf77a0d6e077cb95d466b988c705c6a
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-11 18:51:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -63,7 +63,7 @@ def reduce_to_any_true(source_array)
   result = false
   while i < source_array.length do
     if source_array[i] == true
-    put  result = source_array[i]
+      result = source_array[i]
     end
   i += 1
 end
