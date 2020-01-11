0d0e229b8448e6844dc12ed6829caa636865a229
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-11 17:37:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,7 +40,7 @@ end
 def reduce_to_total(source_array, starting_point = 0)
   i = 0
   while i < source_array.length do
-      result = source_array[i] + result
+      starting_point = source_array[i] + result
       i += 1
   end
 end
