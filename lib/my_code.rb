8a4fa49ab0b7f8a46729283d81b9a715272c799d
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-11 17:18:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,6 +21,6 @@ def map_to_no_change(source_array)
   i = 0
   new_array = []
   while i < source_array.length do
-    new_array << source_array[i].squrt
+    new_array << Math.sqrt(source_array[i])
   end
 end
