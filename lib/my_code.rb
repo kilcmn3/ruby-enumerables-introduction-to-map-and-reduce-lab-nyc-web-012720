9fdf1547f16de5e5731f8e22fb671c66d7bac69a
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-11 17:22:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,7 +21,7 @@ def map_to_double(source_array)
   i = 0
   new_array = []
   while i < source_array.length do
-    new_array << source_array[i] ** 2
+    new_array << source_array[i] * 2
     i += 1
   end
   new_array
