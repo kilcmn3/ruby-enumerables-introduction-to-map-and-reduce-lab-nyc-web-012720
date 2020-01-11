d0d5d242cae9cea406af1eff0fd43df606b46333
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-11 17:33:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,7 +37,7 @@ def map_to_square(source_array)
  new_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array)
   i = 0
   result = 0
   while i < source_array.length do
