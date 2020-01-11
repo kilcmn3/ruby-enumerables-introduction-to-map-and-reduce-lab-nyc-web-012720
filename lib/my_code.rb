4158ff21f91aff8aa82c9937c2aa87a50b73c0d5
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-11 17:19:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,7 +17,7 @@ def map_to_no_change(source_array)
   return new_array
 end
 
-def map_to_no_change(source_array)
+def map_to_double(source_array)
   i = 0
   new_array = []
   while i < source_array.length do
